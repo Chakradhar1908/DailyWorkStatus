905065f2bb2cd56a79738e85e47d806eb858aadb
Chakradhar1908 <chakradhar.deft@gmail.com>
2019-09-27 11:17:04 -0400

    Work status: 09/27/2019

---
+++ WinCDS/Class Modules/clsMailShipTo.vb
@@ -0,0 +1,44 @@
+﻿Public Class clsMailShipTo
+    Public Index As Integer
+    Public Last As String
+    Public First As String
+    Public Address As String
+    Public City As String
+    Public Zip As String
+    Public Tele As String
+    Public PhoneLabel3 As String
+    Public Blank As String
+
+    Private WithEvents mDataAccess As CDataAccess
+    'Implements CDataAccess
+
+    Private Const TABLE_NAME As String = "MailShipTo"
+    Private Const TABLE_INDEX As String = "Index"
+
+    Public Function DataAccess() As CDataAccess
+        DataAccess = mDataAccess
+    End Function
+
+    Public Function Load(ByVal KeyVal As String, Optional ByVal KeyName As String = "") As Boolean
+        ' Checks the database for a matching LeaseNo.
+        ' Returns True if the load was successful, false otherwise.
+        ' If a record was found, also loads the data into this object.
+
+        ' Search for the Style
+        If KeyName = "" Then
+            DataAccess.Records_OpenFieldIndexAtNumber(TABLE_INDEX, KeyVal)
+        ElseIf Left(KeyName, 1) = "#" Then
+            ' This allows searching by AutoNumber - specialized to query by number
+            ' since Access is exceptionally picky about quotation marks.
+            DataAccess.Records_OpenFieldIndexAtNumber(Mid(KeyName, 2), KeyVal)
+        ElseIf Left(KeyName, 1) = "@" Then
+            DataAccess.Records_OpenFieldIndexAtDate(Mid(KeyName, 2), KeyVal)
+        Else
+            DataAccess.Records_OpenFieldIndexAt(KeyName, KeyVal)
+        End If
+
+        ' Move to the first record if we can, and return success.
+        If DataAccess.Records_Available Then Load = True
+    End Function
+
+End Class
